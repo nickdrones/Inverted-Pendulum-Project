30ea0f5483fe7e515a49453b9648dd13363a270c
Nick Belbas <nickbelbas@earthlink.net>
2022-04-25 14:20:59 -0500

    using gyro channel

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,4 +1,11 @@
 /*Example sketch to control a stepper motor with A4988 stepper motor driver and Arduino without a library. More info: https://www.makerguides.com */
+#define WINDOW_SIZE 5
+
+int INDEX = 0;
+int VALUE = 0;
+int SUM = 0;
+int READINGS[WINDOW_SIZE];
+int AVERAGED = 0;
 
 // Define stepper motor connections and steps per revolution:
 #include <Wire.h>
@@ -6,9 +13,16 @@
 
 MPU6050 mpu;
 
+// Timers
+unsigned long timer = 0;
+float timeStep = 0.01;
+
 #define dirPin 2
 #define stepPin 3
 #define stepsPerRevolution 200
+float pitch = 0;
+float roll = 0;
+float yaw = 0;
 
 void setup() {
   // Declare pins as output:
@@ -23,21 +37,37 @@ void setup() {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
   }
+  Serial.println("Calibrating GYRO");
+  mpu.calibrateGyro();
+  Serial.println("Initialize done");
+  mpu.setThreshold(3);
+
+
 }
 
 void loop() {
-  Vector normAccel = mpu.readNormalizeAccel();
+  timer = millis();
+
+  Vector norm = mpu.readNormalizeGyro();
 
   // Calculate Pitch & Roll
-  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis) * 180.0) / M_PI;
+  yaw = yaw + norm.ZAxis * timeStep;
 
   // Output
-  Serial.print(" Roll = ");
-  Serial.print(roll);
-
+  Serial.print(" yaw = ");
+  Serial.print(yaw);
   Serial.println();
+//  Serial.print(" calcpower = ");
+//  Serial.print(calcPower(yaw));
+//  Serial.println();
+  moveStepper(calcPower(yaw));
 
-  //
+
+  //  Serial.println("test");
+  //  for(int i = 0; i<400; i++){
+  //  }
+
+
   //  // Set the spinning direction counterclockwise:
   //  digitalWrite(dirPin, LOW);
   //
@@ -51,7 +81,7 @@ void loop() {
   //  }
   //
   //
-  delay(100);
+  //delay(100);
   //
   //  // Set the spinning direction counterclockwise:
   //  digitalWrite(dirPin, HIGH);
@@ -63,18 +93,49 @@ void loop() {
   //    digitalWrite(stepPin, LOW);
   //    delayMicroseconds(450);
   //  }
-  //
-  //  delay(1000);
+
+  //delay(1000);
+  Serial.println();
+  delay((timeStep * 1000) - (millis() - timer));
+
 }
 
-void moveStepper(int roll) {
-  if (roll < 0) {
-    //something
+
+int calcPower(float sensor) {
+  if (sensor > -50 && sensor < 50) {
+    return sensor * 2;
   }
-  if (roll > 0) {
-    //something
+  else if (sensor < -50) {
+    return -100;
   }
-  if (roll == 0) {
+  else if (sensor > 50) {
+    return 100;
+  }
+}
+//infinity delay is 0, 450 delay is 100
+void moveStepper(int power) {
+  int delaytime = map(abs(power), 0, 100, 1000, 500);
+//  Serial.print(" delaytime = ");
+//  Serial.print(delaytime);
+//  Serial.println();
+  if (power < 0) {  //move left
+    digitalWrite(dirPin, LOW);
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(delaytime);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(delaytime);
+//    Serial.println("left");
+  }
+  if (power > 0) {  //move right
+    digitalWrite(dirPin, HIGH);
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(delaytime);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(delaytime);
+//    Serial.println("right");
+
+  }
+  if (power == 0) {
     return;
   }
 
